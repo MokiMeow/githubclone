69e2f68283f6165602439691a37be1979623afda
MokiMeow <122464960+MokiMeow@users.noreply.github.com>
2024-02-27 19:33:08 +0530

    Update Navbar.tsx

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -51,9 +51,9 @@ return (
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" aria-hidden="true" className="mr-1 absolute right-0 top-[10px] max-lg:hidden"><path fill="none" stroke="#979A9C" opacity=".4" d="M3.5.5h12c1.7 0 3 1.3 3 3v13c0 1.7-1.3 3-3 3h-12c-1.7 0-3-1.3-3-3v-13c0-1.7 1.3-3 3-3z"></path><path fill="#979A9C" d="M11.8 6L8 15.1h-.9L10.8 6h1z"></path></svg>
                         </div>
                         <div className='max-lg:flex justify-center max-lg:border-[1px] max-lg:mr-4 rounded-md max-lg:bg-neutral-800'>
-                            <a href="" className='hover:text-neutral-400 max-lg:py-2 max-lg:text-[16px] max-lg:font-semibold '>Sign in</a>
+                            <a href="https://github.com/login" className='hover:text-neutral-400 max-lg:py-2 max-lg:text-[16px] max-lg:font-semibold '>Sign in</a>
                         </div>
-                        <a href="" className='max-lg:hidden px-2 py-[5px] border-[1px] rounded-md hover:text-neutral-400 '>Sign up</a>
+                        <a href="https://github.com/login" className='max-lg:hidden px-2 py-[5px] border-[1px] rounded-md hover:text-neutral-400 '>Sign up</a>
                     </div>
                 </div>
             </div>
